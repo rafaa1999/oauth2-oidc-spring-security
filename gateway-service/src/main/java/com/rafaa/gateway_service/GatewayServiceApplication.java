bd60cabaaa669d98c1c1d780c712579e85e0d9ba
rafaa1999 <raferhouma10@gmail.com>
2024-08-01 15:23:19 +0100

    Add security part usin keycloack

--- gateway-service/src/main/java/com/rafaa/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/rafaa/gateway_service/GatewayServiceApplication.java
@@ -2,6 +2,10 @@ package com.rafaa.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,6 +15,14 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class);
+	}
+
+	@Bean
+	SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http
+				.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+				.oauth2Login(Customizer.withDefaults())
+				.build();
 	}
 
 }
